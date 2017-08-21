6970cec5a85e9660e5b8012e932bf44d5227be5f
Christopher L. Farrow <cfarrow@enthought.com>
2017-08-21 11:43:45 -0500

    MAINT: Refactoring tests to reflect success or error with new or existing files.

--- sda/tests/test_sda_file.py
+++ sda/tests/test_sda_file.py
@@ -5,143 +5,53 @@ from sda.sda_file import SDAFile
 from sda.testing import BAD_ATTRS, GOOD_ATTRS, temporary_file, temporary_h5file
 from sda.utils import write_header
 
+# TODO - test actual read and write
+
 
 class TestSDAFile(unittest.TestCase):
 
     def test_init_r(self):
-
-        # No file
-        with temporary_file() as name:
-            pass  # file is deleted after this point
-        with self.assertRaises(IOError):
-            SDAFile(name, 'r')
-
-        # Uninitialized -> BadSDAFile
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.close()
-            with self.assertRaises(BadSDAFile):
-                SDAFile(name, 'r')
-
-        # nonexistant -> IOError
-        with self.assertRaises(IOError):
-            SDAFile(name, 'r')
-
-        # Test good attrs
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.attrs.update(GOOD_ATTRS)
-            h5file.close()
-            sda_file = SDAFile(name, 'r')
-            self.assertHeader(sda_file, GOOD_ATTRS)
-
-        # Test bad attrs
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.attrs.update(BAD_ATTRS)
-            h5file.close()
-            with self.assertRaises(BadSDAFile):
-                SDAFile(name, 'r')
+        self.assertInitNew('r', exc=IOError)
+        self.assertInitExisting('r', {}, BadSDAFile)
+        self.assertInitExisting('r', BAD_ATTRS, BadSDAFile)
+        self.assertInitExisting('r', GOOD_ATTRS)
 
     def test_init_r_plus(self):
-        # No file
-        with temporary_file() as name:
-            pass  # file is deleted after this point
-        with self.assertRaises(IOError):
-            SDAFile(name, 'r+')
-
-        # Uninitialized -> BadSDAFile
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.close()
-            with self.assertRaises(BadSDAFile):
-                SDAFile(name, 'r+')
-
-        # nonexistant -> IOError
-        with self.assertRaises(IOError):
-            SDAFile(name, 'r+')
-
-        # Test good attrs
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.attrs.update(GOOD_ATTRS)
-            h5file.close()
-            sda_file = SDAFile(name, 'r+')
-            self.assertHeader(sda_file, GOOD_ATTRS)
-
-        # Test bad attrs
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.attrs.update(BAD_ATTRS)
-            h5file.close()
-            with self.assertRaises(BadSDAFile):
-                SDAFile(name, 'r+')
+        self.assertInitNew('r+', exc=IOError)
+        self.assertInitExisting('r+', exc=BadSDAFile)
+        self.assertInitExisting('r+', {}, BadSDAFile)
+        self.assertInitExisting('r+', BAD_ATTRS, BadSDAFile)
+        self.assertInitExisting('r+', GOOD_ATTRS)
 
     def test_init_w(self):
-        default_attrs = {}
-        write_header(default_attrs)
+        attrs = {}
+        write_header(attrs)
+        self.assertInitNew('w', attrs)
+        self.assertInitExisting('w', attrs)
+        self.assertInitExisting('w', {})
 
-        # No file, no problem
-        with temporary_file() as name:
-            pass  # file is deleted after this point
-        sda_file = SDAFile(name, 'w')
-        self.assertHeader(sda_file, default_attrs)
+    def test_init_x(self):
+        attrs = {}
+        write_header(attrs)
 
-        # Yes file, uninitialized, no problem
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.close()
-            sda_file = SDAFile(name, 'w')
-            self.assertHeader(sda_file, default_attrs)
-
-            # Yes file, initialized, no problem
-            sda_file = SDAFile(name, 'w')
-            self.assertHeader(sda_file, default_attrs)
-
-    def test_init_x(self, mode='x'):
-        default_attrs = {}
-        write_header(default_attrs)
-
-        # No file, no problem
-        with temporary_file() as name:
-            pass  # file is deleted after this point
-        sda_file = SDAFile(name, mode)
-        self.assertHeader(sda_file, default_attrs)
-
-        # Yes file, error
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.close()
-            with self.assertRaises(IOError):
-                sda_file = SDAFile(name, mode)
+        self.assertInitNew('x', attrs)
+        self.assertInitExisting('x', exc=IOError)
 
     def test_init_w_minus(self):
-        self.test_init_x('w-')
+        attrs = {}
+        write_header(attrs)
+
+        self.assertInitNew('w-', attrs)
+        self.assertInitExisting('w-', exc=IOError)
 
     def test_init_a(self):
-        default_attrs = {}
-        write_header(default_attrs)
+        attrs = {}
+        write_header(attrs)
 
-        # No file, no problem
-        with temporary_file() as name:
-            pass  # file is deleted after this point
-        sda_file = SDAFile(name, 'a')
-        self.assertHeader(sda_file, default_attrs)
-
-        # Uninitialized -> BadSDAFile
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.close()
-            with self.assertRaises(BadSDAFile):
-                sda_file = SDAFile(name, 'a')
-
-        # Initialized, no problem, header and data is preserved
-        with temporary_h5file() as h5file:
-            name = h5file.filename
-            h5file.attrs.update(GOOD_ATTRS)
-            h5file.close()
-            sda_file = SDAFile(name, 'a')
-            self.assertHeader(sda_file, GOOD_ATTRS)
+        self.assertInitNew('a', attrs)
+        self.assertInitExisting('a', GOOD_ATTRS)
+        self.assertInitExisting('a', BAD_ATTRS, BadSDAFile)
+        self.assertInitExisting('a', {}, BadSDAFile)
 
     def test_init_default(self):
         with temporary_file() as name:
@@ -149,7 +59,38 @@ class TestSDAFile(unittest.TestCase):
         sda_file = SDAFile(name)
         self.assertEqual(sda_file.mode, 'a')
 
-    def assertHeader(self, sda_file, attrs):
+    def assertAttrs(self, sda_file, attrs):
         for attr, expected in attrs.items():
             actual = getattr(sda_file, attr)
             self.assertEqual(actual, expected)
+
+    def assertInitExisting(self, mode, attrs={}, exc=None):
+        """ Assert attributes or error when init with existing file.
+
+        Passed ``attrs`` are used when creating the existing file. When ``exc``
+        is None, this also tests that the ``attrs`` are preserved.
+
+        """
+        with temporary_h5file() as h5file:
+            name = h5file.filename
+            h5file.attrs.update(attrs)
+            h5file.close()
+
+            if exc is not None:
+                with self.assertRaises(exc):
+                    SDAFile(name, mode)
+            else:
+                sda_file = SDAFile(name, mode)
+                self.assertAttrs(sda_file, attrs)
+
+    def assertInitNew(self, mode, attrs={}, exc=None):
+        """ Assert attributes or error when init with non-existing file. """
+        with temporary_file() as name:
+            pass  # file is deleted after this point
+
+        if exc is not None:
+            with self.assertRaises(exc):
+                SDAFile(name, mode)
+        else:
+            sda_file = SDAFile(name, mode)
+            self.assertAttrs(sda_file, attrs)
