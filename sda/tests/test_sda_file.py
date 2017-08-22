84613dda1e5f0c5906ec6c307f1ad26d23f262c9
Christopher L. Farrow <cfarrow@enthought.com>
2017-08-22 14:57:35 -0500

    MAINT: clarifying some test names.

--- sda/tests/test_sda_file.py
+++ sda/tests/test_sda_file.py
@@ -17,38 +17,38 @@ from sda.utils import write_header
 
 class TestSDAFileInit(unittest.TestCase):
 
-    def test_init_r(self):
+    def test_mode_r(self):
         self.assertInitNew('r', exc=IOError)
         self.assertInitExisting('r', {}, BadSDAFile)
         self.assertInitExisting('r', BAD_ATTRS, BadSDAFile)
         self.assertInitExisting('r', GOOD_ATTRS)
 
-    def test_init_r_plus(self):
+    def test_mode_r_plus(self):
         self.assertInitNew('r+', exc=IOError)
         self.assertInitExisting('r+', exc=BadSDAFile)
         self.assertInitExisting('r+', exc=BadSDAFile)
         self.assertInitExisting('r+', BAD_ATTRS, BadSDAFile)
         self.assertInitExisting('r+', GOOD_ATTRS)
 
-    def test_init_w(self):
+    def test_mode_w(self):
         self.assertInitNew('w')
         self.assertInitExisting('w')
 
-    def test_init_x(self):
+    def test_mode_x(self):
         self.assertInitNew('x')
         self.assertInitExisting('x', exc=IOError)
 
-    def test_init_w_minus(self):
+    def test_mode_w_minus(self):
         self.assertInitNew('w-')
         self.assertInitExisting('w-', exc=IOError)
 
-    def test_init_a(self):
+    def test_mode_a(self):
         self.assertInitNew('a')
         self.assertInitExisting('a', GOOD_ATTRS)
         self.assertInitExisting('a', BAD_ATTRS, BadSDAFile)
         self.assertInitExisting('a', {}, BadSDAFile)
 
-    def test_init_default(self):
+    def test_mode_default(self):
         with temporary_h5file() as h5file:
             name = h5file.filename
             h5file.attrs.update(GOOD_ATTRS)
@@ -56,7 +56,7 @@ class TestSDAFileInit(unittest.TestCase):
             sda_file = SDAFile(name)
             self.assertEqual(sda_file.mode, 'a')
 
-    def test_init_kw(self):
+    def test_pass_kw(self):
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w', driver='core')
             with sda_file._h5file('r') as h5file:
@@ -158,7 +158,7 @@ class TestSDAFileInsert(unittest.TestCase):
             with self.assertRaises(ValueError):
                 sda_file.insert('test', [1, 2, 3])
 
-    def test_insert_character_scalar(self):
+    def test_character_scalar(self):
         values = (obj for (obj, typ) in TEST_SCALARS if typ == 'character')
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
@@ -184,7 +184,7 @@ class TestSDAFileInsert(unittest.TestCase):
                 sda_file, 'character', label, deflate, 'yes', None
             )
 
-    def test_insert_logical_array(self):
+    def test_logical_array(self):
         values = (obj for (obj, typ) in TEST_ARRAYS if typ == 'logical')
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
@@ -203,7 +203,7 @@ class TestSDAFileInsert(unittest.TestCase):
             # Make sure the 'Modified' attr gets updated
             self.assertNotEqual(sda_file.Modified, 'Unmodified')
 
-    def test_insert_logical_scalar(self):
+    def test_logical_scalar(self):
         values = (obj for (obj, typ) in TEST_SCALARS if typ == 'logical')
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
@@ -222,7 +222,7 @@ class TestSDAFileInsert(unittest.TestCase):
             # Make sure the 'Modified' attr gets updated
             self.assertNotEqual(sda_file.Modified, 'Unmodified')
 
-    def test_insert_numeric_array(self):
+    def test_numeric_array(self):
         values = (obj for (obj, typ) in TEST_ARRAYS if typ == 'numeric')
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
@@ -246,7 +246,7 @@ class TestSDAFileInsert(unittest.TestCase):
             sda_file.insert(label, [], label, deflate)
             self.assertRecord(sda_file, 'numeric', label, deflate, 'yes', None)
 
-    def test_insert_numeric_scalar(self):
+    def test_numeric_scalar(self):
         values = (obj for (obj, typ) in TEST_SCALARS if typ == 'numeric')
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
@@ -269,7 +269,7 @@ class TestSDAFileInsert(unittest.TestCase):
             sda_file.insert(label, np.nan, label, deflate)
             self.assertRecord(sda_file, 'numeric', label, deflate, 'yes', None)
 
-    def test_insert_unsupported(self):
+    def test_unsupported(self):
         with temporary_file() as file_path:
             sda_file = SDAFile(file_path, 'w')
             with sda_file._h5file('a') as h5file:
